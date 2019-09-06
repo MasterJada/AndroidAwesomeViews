d9fb9b5952735fb32ea7bb18b0add1308986d619
Oleg <oleg.r@didww.com>
2019-09-06 15:13:59 +0300

    Add bash

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir report/test-results
-modules=("app" "common")
-for module in "${modules[@]}"
-do
-
-    testsDir=""
-    testsDir="$module/build/test-results/testDebugUnitTest"
-
-
-    if [ ! "$(ls -A $testsDir)" ]; then
-        echo "Unit tests report wasn't found for module: $module"
-        continue
-    fi
-
-    # copy all files inside, to our folder
-    cp $testsDir/* report/test-results/
-
-done
+mkdir target
+mkdir target/test-reports
